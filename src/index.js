8626d4b8c4f866765664cfcaecc3ecf2f445d35c
xct007 <hackerxct007@gmail.com>
2022-11-08 16:40:24 +0700

    Add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import { main } from "./helper/connect.js";
 import readCommand from "./helper/readCommand.js";
-//
+import express from 'express'
+
+async function server() {
+    const app = express()
+    app.use("/", (req, res) => {
+        res.send({
+            "test": "test"
+        })
+    })
+
+}
+
+server()
 await readCommand();
 main();
