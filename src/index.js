6f66b7df6c2530c8d4bbc553e7002ca5bc2002c4
xct007 <hackerxct007@gmail.com>
2022-11-08 16:44:30 +0700

    Add

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import express from 'express'
 
 async function server() {
     const app = express()
+    const port = 3000;
+
     app.use("/", (req, res) => {
         res.send({
             "test": "test"
         })
     })
 
+    app.listen(port, () => {
+        console.log(`Running on http://localhost:${port}`)
+    })
 }
 
 server()
