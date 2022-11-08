12531a0d0ec3baa4ca77b459c98f9d7e00091b59
xct007 <hackerxct007@gmail.com>
2022-11-08 17:01:21 +0700

    Add

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import { main } from "./helper/connect.js";
 import readCommand from "./helper/readCommand.js";
 
-import express from 'express'
-import { createServer } from 'http'
+import express from "express";
+import { createServer } from "http";
 
 async function server() {
-    const app = express()
-    const server = createServer(app)
-    const port = 3000;
+	const app = express();
+	const server = createServer(app);
+	const port = 3000;
 
-    app.get("/", (req, res) => {
-        res.send({
-            "test": "test"
-        })
-    })
+	app.get("/", (req, res) => {
+		res.send({
+			test: "test",
+		});
+	});
 
-    server.listen(port, () => {
-        console.log(`Running on http://localhost:${port}`)
-    })
+	server.listen(port, async () => {
+		console.log(`Running on http://localhost:${port}`);
+		await readCommand();
+
+		main();
+	});
 }
 
-server()
-await readCommand();
-main();
+server();
